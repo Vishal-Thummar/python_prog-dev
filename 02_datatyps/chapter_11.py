794400445cf6ee950d6159cd0f39e28b5bd682b0
Vishal Thummar <114647476+Vishal-Thummar@users.noreply.github.com>
2026-06-01 11:29:26 +0530

    Implement order and recipe management with detailed print statements

---
+++ 02_datatyps/chapter_11.py
@@ -0,0 +1,13 @@
+# import arrow
+# brewing_time = arrow.utcnow()
+# brewing_time.to("Europe/Rome")
+
+# from collections import namedtuple
+# chai_profie =  namedtuple("ChaiProfile", ["sweetness", "milk_level"])
+# print(chai_profie)
+
+
+a = 5
+b = 5
+print(id(a))
+print(id(b))
